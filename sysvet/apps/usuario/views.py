39d1854b89aa7b9076b61b19b9c1cb586893e371
unknown <familiajoselo@gmail.com>
2021-01-24 15:26:48 -0300

    Se agrega comentarios

--- sysvet/apps/usuario/views.py
+++ sysvet/apps/usuario/views.py
@@ -54,6 +54,9 @@ def logoutUser(request):
     return redirect('/accounts/login/')
 
 @login_required()
+'''
+    @login_required(): Decorador que permite que no se ingrese al home desde la url sin estar logeado
+'''
 def home_user(request):
     """[summary]
 
