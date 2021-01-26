c99d52f889da09ec0f4bd9ea3bfb0a2c78c004f5
unknown <familiajoselo@gmail.com>
2021-01-25 23:33:02 -0300

    Avances en vistas de clientes

--- sysvet/apps/cliente/views.py
+++ sysvet/apps/cliente/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .forms import ClienteForm
+from .views import Cliente
+
+#Metodo para agregar cliente
+@login_required()
+def add_cliente(request):
+    form = ClienteForm
+    if request.method == 'POST':
+        form = ClienteForm(request.POST or None)
+        if form.is_valid():
+            form.save:
+            return redirect('/cliente/list')
+
+    context = {'form' : form}
+    return render(request, 'cliente/add_cliente.html', context)
+
