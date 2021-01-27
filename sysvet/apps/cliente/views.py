b3f624dc72e63d15c2e021a99a0160563485fa6b
unknown <familiajoselo@gmail.com>
2021-01-26 23:16:52 -0300

    avances en form de cliente

--- sysvet/apps/cliente/views.py
+++ sysvet/apps/cliente/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import ClienteForm
-from .views import Cliente
+from .models import Cliente
 
 #Metodo para agregar cliente
 @login_required()
@@ -11,9 +11,19 @@ def add_cliente(request):
     if request.method == 'POST':
         form = ClienteForm(request.POST or None)
         if form.is_valid():
-            form.save:
+            form.save()
             return redirect('/cliente/list')
 
     context = {'form' : form}
+    print("entro")
     return render(request, 'cliente/add_cliente.html', context)
+
+#Metodo para listar todos los clientes
+@login_required()
+def list_clientes(request):
+    clientes = Cliente.objects.all()
+    context = {'clientes' : clientes}
+    return render(request, "cliente/list_cliente.html", context)
+
+
 
