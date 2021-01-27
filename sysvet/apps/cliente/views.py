79f3958e16013fb6c9ec50c87417f4d6be651800
unknown <familiajoselo@gmail.com>
2021-01-27 20:41:14 -0300

    Se agrega cuidades

--- sysvet/apps/cliente/views.py
+++ sysvet/apps/cliente/views.py
@@ -2,20 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import ClienteForm
-from .models import Cliente
+from .models import Cliente, Ciudad
+
 
 #Metodo para agregar cliente
 @login_required()
 def add_cliente(request):
     form = ClienteForm
+    cuidad = Ciudad.objects.all()
     if request.method == 'POST':
         form = ClienteForm(request.POST or None)
         if form.is_valid():
             form.save()
-            return redirect('/cliente/list')
+            return redirect('/')
 
-    context = {'form' : form}
-    print("entro")
+    context = {'form' : form, 'cuidad' : cuidad}
     return render(request, 'cliente/add_cliente.html', context)
 
 #Metodo para listar todos los clientes
