199af3ffdb7127ca7c0e6b11b84093a94ae8342e
unknown <familiajoselo@gmail.com>
2021-04-01 00:48:34 -0400

    se agrega correcciones

--- sysvet/static/project_static/vendors/validator/validator.js
+++ sysvet/static/project_static/vendors/validator/validator.js
@@ -31,24 +31,25 @@
     FormValidator.prototype = {
         // Validation error texts
         texts : {
-            invalid         : 'inupt is not as expected',
-            short           : 'input is too short',
-            long            : 'input is too long',
-            checked         : 'must be checked',
-            empty           : 'please put something here',
-            select          : 'Please select an option',
-            number_min      : 'too low',
-            number_max      : 'too high',
-            url             : 'invalid URL',
-            number          : 'not a number',
-            email           : 'email address is invalid',
-            email_repeat    : 'emails do not match',
-            date            : 'invalid date',
-            time            : 'invalid time',
-            password_repeat : 'passwords do not match',
-            no_match        : 'no match',
-            complete        : 'input is not complete'
+            invalid         : 'Entrada no esperada',
+            short           : 'Entrada muy corta',
+            long            : 'Entrada muy larga',
+            checked         : 'Chequeado',
+            empty           : 'Campo vacio',
+            select          : 'Seleccione una opción',
+            number_min      : 'Muy corto',
+            number_max      : 'Muy largo',
+            url             : 'URL invalido',
+            number          : 'No es un numero valido',
+            email           : 'email no valido',
+            email_repeat    : 'emails no coinciden',
+            date            : 'Fecha invalido',
+            time            : 'Tiempo invalido',
+            password_repeat : 'No coinciden',
+            no_match        : 'No coinciden',
+            complete        : 'No esta completo'
         },
+
 
         // default settings
         defaults : {
