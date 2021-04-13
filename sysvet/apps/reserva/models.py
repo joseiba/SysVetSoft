f948c6b9a44d1836c2a6cb2200e9dbc4d427df03
unknown <familiajoselo@gmail.com>
2021-04-12 23:03:00 -0400

    se agrega modelo de reserva

--- sysvet/apps/reserva/models.py
+++ sysvet/apps/reserva/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from apps.ventas.mascota.models import Mascota
+
+
 # Create your models here.
 class Servicio(models.Model):
     """
@@ -22,3 +25,26 @@ class Servicio(models.Model):
     def get_absolute_url(self):
         """Retorna el URL para acceder a una instancia de una ciudad en particular."""
         return reverse('servicio-detail', args=[str(self.id)])
+
+
+class Reserva(models.Model):
+    """
+    Clase que define la estructura de la reserva
+    """
+    descripcion = models.CharField(max_length=200, help_text = "Ingrese la descripción del la reserva", blank=True, null=True)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    id_servicio = models.ForeignKey('Servicio', on_delete=models.CASCADE, null=False)
+    id_mascota = models.ForeignKey(Mascota, on_delete=models.CASCADE, null=False) 
+
+    class Meta:
+        verbose_name = "Reserva"
+        verbose_name_plural = "Reservas"
+
+    def __str__(self):
+        """Formato de reserva"""
+        return '{0}'.format(self.id_mascota.nombre_mascota)
+
+    def get_absolute_url(self):
+        """Retorna el URL para acceder a una instancia de una reserva en particular."""
+        return reverse('reserva-detail', args=[str(self.id)])
